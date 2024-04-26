86b30476eb8d32205655cde674ec50e2d90538e6
root <root@DESKTOP-BKCHLNB>
2024-04-25 20:47:57 -0400

    unit 8

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const validateForm = (e) => {
     for(let i = 0; i < 3; i++){
         if(petitionInputs[i].value.length < 2){
             containsErrors = true;
-            petitionInputs[i].classList.add('error');
+            titiopenInputs[i].classList.add('error');
         }
         else{
             petitionInputs[i].classList.remove('error');    
@@ -75,6 +75,40 @@ const addSignature = (e) => {
 //sign_button.addEventListener('submit', addSignature);
 // TODO: Remove the click event listener that calls addSignature()
 
+//Unit 8: Animation
+let animation = {
+    revealDistance: 150,
+    initialOpacity: 0,
+    transitionDelay: 0, 
+    transitionDuration: '2s',
+    transitionProperty: 'all',
+    trnasitionTimingFunction: 'ease'
+}  
 
-  
-  
+let revealableContainers = document.getElementsByClassName('revealable')
+
+const reveal = () => {
+    for(let i = 0; i < revealableContainers.length; i++){
+        let windowHeight = window.innerHeight;
+        let topOfRevealableContainer = revealableContainers[i].getBoundingClientRect().top;
+        if (topOfRevealableContainer < windowHeight - animation.revealDistance) {
+            /* add the active class to the revealableContainer's classlist */
+            revealableContainers[i].classList.add('action');
+        }else {
+            /* remove the active class to the revealableContainer's classlist */
+            revealableContainers[i].classList.remove('action');
+        }
+
+    }
+}
+let reduceMotionButton = document.getElementById('reduce-motion')
+const reduceMotion = () =>{
+    animation.transitionDelay = 25;
+    for(let i = 0; i < revealableContainers.length; i++){
+        
+        revealableContainers[i].style.transitionDelay = animation.transitionDelay;
+    }
+}
+reduceMotionButton.addEventListener("click",reduceMotion)
+window.addEventListener("scroll",reveal)
+
